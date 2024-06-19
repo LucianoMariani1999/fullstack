e4a7d08788b3791347b813011a6deb9339b76184
Lucho <lucho.mariani99@gmail.com>
2024-06-19 20:42:04 -0300

    Subo el trabajo actualizado

---
+++ 04-introjavascript/index.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+/* comentarios
+de
+varias
+líneas
+en JS
+ */
+
+// let hola = 'pepe'
+
+ /* 
+  let hola = 'pepe'
+ let hola = 'pepe'
+ let hola = 'pepe'
+ let hola = 'pepe'
+ let hola = 'pepe'
+ let hola = 'pepe'
+ let hola = 'pepe'
+ let hola = 'pepe'
+ let hola = 'pepe'
+  */
+
+ //variables: espacios de memoria para almacenar datos
+//1. con var
+//palabras reservadas: son utilizados sólo para las definiciones del lenguaje de programación
+var nombre;
+var email
+var edad;
+var país
+
+
+
+
+
+
+
+
